51c87462924ef54ea88d183cebc6525d3b111178
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-07-05 09:28:47 -0500

    added service workers

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const APP_PREFIX = 'FoodFest-'
+const VERSION = 'version_01'
+const CACHE_NAME = APP_PREFIX + VERSION
+
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./events.html",
+    "./tickets.html",
+    "./schedule.html",
+    "./assets/css/style.css",
+    "./assets/css/bootstrap.css",
+    "./assets/css/tickets.css",
+    "./dist/app.bundle.js",
+    "./dist/events.bundle.js",
+    "./dist/tickets.bundle.js",
+    "./dist/schedule.bundle.js"
+]
+
+self.addEventListener("install", function(e){
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log(`installing cache: ${CACHE_NAME}`)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
+
+self.addEventListener('activate', function(e){
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            let cacheKeepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX)
+            })
+            cacheKeepList.push(CACHE_NAME)
+
+            return Promise.all(
+                keyList.map(function(key, i){
+                    if(cacheKeepList.indexOf(key) === -1) {
+                        console.log(`deleteing cache: ${keyList[i]}`)
+                        return caches.delete(keyList[i])
+                    }
+                })
+            )
+        })
+    )
+})
